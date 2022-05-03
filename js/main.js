75ddad748b404496256099ca0e94b0b8c3e4272f
AlessP93 <alespiscino@libero.it>
2022-05-03 15:02:58 +0200

    aggiunta struttura

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Chiedi all’utente la sua email
+const email = prompt("Inserisci la tua email")
+// controlla che sia nella lista di chi può accedere
+// stampa un messaggio appropriato sull’esito del controllo
